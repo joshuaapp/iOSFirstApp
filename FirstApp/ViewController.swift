299036b537265f8b208a5b019a3f9513205d5219
joshuaapp <joshua.apperley@gmail.com>
2016-09-23 11:20:10 +1200

    added popup box that notifies player if they have won/lost when the max score limit has been reached

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -17,17 +17,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerScoreLabel: UILabel!
     @IBOutlet weak var enemyScoreLabel: UILabel!
     @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var popUpMessage: UITextField!
     
     var playerScore = 0
     var enemyScore = 0
-    var gameWon = false;
+    var gameWon = false
+    let winningScore = 5
     
     let cardNames:[String] = ["card2", "card3", "card4","card5","card6","card7","card8","card9","card10","jack","queen","king","ace"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.popUpView.isHidden = true
+        self.popUpView.layer.cornerRadius = 5
+        self.popUpView.layer.shadowOpacity = 0.8
+        self.popUpView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
         //additional setup
-        //self.dealButton.setTitle("Deal Cards", forState: UIControlState.Normal)
     }
     
     override func didReceiveMemoryWarning() {
@@ -57,7 +63,7 @@ class ViewController: UIViewController {
                     //both cards are same value, therefore do nothing
                 }            }
                 
-            //if player picks card2
+                //if player picks card2
             else if(sender.isEqual(self.secondCard)){
                 transition(from: secondCard, too: cardNames[secondCardRandNum])
                 //compare two cards to determine winner
@@ -79,12 +85,8 @@ class ViewController: UIViewController {
             }
             else if(sender.isEqual(secondCard)){
                 transition(from: firstCard, too: cardNames[firstCardRandNum])
-
+                
             }
-            
-            //transition(from: firstCard, too: "back")
-            //transition(from: secondCard, too: "back")
-            
         }
         checkGameWon();
     }
@@ -94,13 +96,51 @@ class ViewController: UIViewController {
             from.setImage(UIImage(named: too), for: .normal)
             }, completion: nil)
     }
+    
     func checkGameWon(){
-        if(self.playerScore == 10 || self.enemyScore == 10){
+        if(self.playerScore == winningScore || self.enemyScore == winningScore){
             gameWon = true
-            //reset scores
-            self.playerScore=0
-            self.enemyScore=0
+            if(self.playerScore == winningScore){
+                showPopup(self.popUpView, withMessage: "You have won!", animated: true)
+            }
+            else if(self.enemyScore == winningScore){
+                showPopup(self.popUpView, withMessage: "Oh no, you have lost!", animated: true)
+            }
         }
+    }
+    
+    /**Functions for winning/losing popup box**/
+    open func showPopup(_ aView: UIView!, withMessage message: String!, animated: Bool)
+    {
+        aView.isHidden = false
+        popUpMessage!.text = message
+        if animated
+        {
+            self.showAnimate()
+        }
+    }
+    
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    @IBAction open func closePopup(_ sender: AnyObject) {
+        self.popUpView.isHidden = true
+        //reset scores
+        self.playerScore=0
+        self.enemyScore=0
+        //update text
+        self.enemyScoreLabel.text = String(enemyScore)
+        self.playerScoreLabel.text = String(playerScore)
+        //reset cards
+        transition(from: firstCard, too: "back")
+        transition(from: secondCard, too: "back")
     }
     
 }
